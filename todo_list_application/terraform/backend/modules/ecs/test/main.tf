e16d7e70be689f89e7579f11e1b35b37c40808c2
setsumaru1992 <setsumaru1992@gmail.com>
2024-09-21 23:44:36 +0900

    ecs環境をterraformで記述

---
+++ todo_list_application/terraform/backend/modules/ecs/test/main.tf
@@ -0,0 +1,17 @@
+variable "ecr_registry_name" { type = string }
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "network" {
+  source = "../../network"
+}
+
+module "ecs" {
+  source = "../"
+
+  ecr_registry_name = var.ecr_registry_name
+  vpc_id = module.network.vpc_id
+  subnet_ids = module.network.subnet_ids
+}
---
+++ todo_list_application/terraform/backend/modules/network/main.tf
@@ -0,0 +1,51 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+output "vpc_id" {
+  value       = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value       = [aws_subnet.main.id]
+}
