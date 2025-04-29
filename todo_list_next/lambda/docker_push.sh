a1ddf09807472f66ab6314dd26a04d3bbb520d9a
setsumaru1992 <setsumaru1992@gmail.com>
2025-04-29 20:48:27 +0900

    lambdaをterraformで立てるために手動lambda用イメージプッシュスクリプトを修正

--- todo_list_next/lambda/docker_push.sh
+++ todo_list_next/lambda/docker_push.sh
@@ -7,7 +7,8 @@ cd $script_parent_dir
 
 export $(grep -v '^#' ../.env | xargs)
 
-image_name_with_tag=todo_app_front_lambda:latest
+#image_name_with_tag=todo_app_front_lambda:latest
+image_name_with_tag=todo_app/front/lambda/managed_by_terraform/dev:latest
 docker build -t ${image_name_with_tag} -f lambda/Dockerfile .
 
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin ${IMAGE_REGISTORY}
